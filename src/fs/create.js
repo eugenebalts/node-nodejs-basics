607f2319f1259065b29229cfb030d51713ebd495
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-26 20:42:26 +0100

    feat: implement create method

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { readFile, mkdir, writeFile } from 'node:fs/promises';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const endpoint = path.resolve('src', 'fs', 'files', 'fresh.txt');
+  const errorMsg = 'FS operation failed';
+
+  try {
+    await readFile(endpoint, 'utf8');
+
+    throw new Error(errorMsg);
+  } catch (err) {
+    if (err instanceof Error && err.message === errorMsg) {
+      console.error(err);
+    }
+
+    try {
+      await mkdir(path.dirname(endpoint), { recursive: true });
+
+      await writeFile(path.dirname(endpoint), 'I am fresh and young', 'utf8');
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 };
 
 await create();
