4d9c93f145f4d1e6881b26889d72253f1a2d2463
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-27 14:45:18 +0100

    feat: implement list method

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+  const pathToCurDir = path.dirname(fileURLToPath(import.meta.url));
+  const pathToSourceDir = path.join(pathToCurDir, 'files');
+  const errorMsg = 'FS operation failed';
+
+  try {
+    const files = await readdir(pathToSourceDir);
+
+    files.forEach((file) => {
+      console.log(path.basename(file));
+    });
+  } catch (err) {
+    throw new Error(errorMsg);
+  }
 };
 
 await list();
