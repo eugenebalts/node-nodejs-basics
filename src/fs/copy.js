8d7f66465c5de1d3c56689bd3778f08a7be391de
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-27 13:44:40 +0100

    feat: implement copy method

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,47 @@
+import { access, mkdir, copyFile, readdir } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const copy = async () => {
-    // Write your code here 
+  const pathToCurDir = path.dirname(fileURLToPath(import.meta.url));
+  const pathToSourceDir = path.join(pathToCurDir, 'files');
+  const pathToTargetDir = path.join(pathToCurDir, 'files-copy');
+  const errorMsg = 'FS operation failed';
+
+  try {
+    await access(pathToTargetDir);
+
+    throw new Error(errorMsg);
+  } catch (err) {
+    if (err instanceof Error && err.message === errorMsg) {
+      console.error(err);
+
+      return;
+    }
+
+    await copyFiles(pathToSourceDir, pathToTargetDir);
+  }
+};
+
+const copyFiles = async (pathToSourceDir, pathToTargetDir) => {
+  try {
+    const sourceFiles = await readdir(pathToSourceDir);
+
+    await mkdir(pathToTargetDir);
+
+    for (const fileToCopy of sourceFiles) {
+      const pathToSourceFile = path.join(pathToSourceDir, fileToCopy);
+      const pathToTargetFile = path.join(pathToTargetDir, fileToCopy);
+
+      if (!!path.extname(pathToSourceFile)) {
+        copyFile(pathToSourceFile, pathToTargetFile);
+      } else {
+        await copyFiles(pathToSourceFile, pathToTargetFile);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await copy();
