d7ad3365b46c286063a3b99dea5ade101a4773fc
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-27 14:28:52 +0100

    feat: implement delete method

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { rm } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+  const pathToCurDir = path.dirname(fileURLToPath(import.meta.url));
+  const pathToSourceFile = path.join(pathToCurDir, 'files', 'fileToRemove.txt');
+  const errorMsg = 'FS operation failed';
+
+  try {
+    await rm(pathToSourceFile);
+  } catch (err) {
+    throw new Error(errorMsg);
+  }
 };
 
 await remove();
