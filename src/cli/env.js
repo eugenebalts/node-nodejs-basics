59cdf52b2368a2467130a0a7c3180d57098367f2
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-28 13:25:31 +0100

    feat: implement env parser

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const envRssVars = Object.entries(env).filter(([key]) =>
+    key.startsWith('RSS_')
+  );
+
+  const result = envRssVars.map(([key, value]) => `${key}=${value}`).join('; ');
+
+  console.log(result);
 };
 
 parseEnv();
