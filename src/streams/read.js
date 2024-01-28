1a95e0b301545aca13dd6ff17cd4fb606b9fb0f5
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-28 20:10:55 +0100

    feat: implement readStream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathToSourceFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  const readSteam = createReadStream(pathToSourceFile, 'utf-8');
+
+  readSteam.on('data', (chunk) => {
+    stdout.write(chunk);
+  });
+
+  readSteam.on('error', (err) => {
+    throw new Error(err.message);
+  });
 };
 
 await read();
