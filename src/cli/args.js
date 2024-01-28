16bc2028da6dcb036fb9a9ca78210512bade74fe
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-28 13:44:10 +0100

    feat: implement args parser

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const processArgs = process.argv.slice(2);
+
+  console.log(
+    processArgs
+      .reduce((acc, cur, index) => {
+        const newString = index % 2 === 0 ? `${cur.slice(2)} is ` : `${cur}, `;
+
+        return acc + newString;
+      }, '')
+      .trim()
+      .slice(0, -1)
+  );
 };
 
 parseArgs();
