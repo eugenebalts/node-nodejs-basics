52f73604d0eba70e98aaaa8447d7ea62c785f4e4
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-29 18:11:14 +0100

    feat: implement zlib-streams: compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToSourceFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+const pathToDestination = path.join(__dirname, 'files', 'archive.txt.gz');
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    await pipeline(
+      createReadStream(pathToSourceFile),
+      createGzip(),
+      createWriteStream(pathToDestination)
+    );
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 await compress();
